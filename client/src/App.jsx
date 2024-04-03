bb3545d0f22403819d7757de881dd64e3ead343e
Mawasi1 <nj.mawasi@hotmail.com>
2024-04-03 21:48:07 +0300

    updated report page with service provider details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile.jsx";
 import CreateReport from "./pages/CreateReport";
 import ReportPage from "./pages/ReportPage";
+import EditProfile from "./pages/EditProfile";
 
 const App = () => {
   const isAuthenticated = () => {
@@ -40,6 +41,7 @@ const App = () => {
           element={isAuthenticated() ? <CreateReport /> : <Navigate to="/sign-in" />}
         />
         <Route path="/reports/:id" element={<ReportPage />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </BrowserRouter>
   );
