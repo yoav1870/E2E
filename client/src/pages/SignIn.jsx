3fb5c99f0ca9d52ba21dd510801b871b94e1d70c
noy2611 <noyr2611@gmail.com>
2024-04-03 21:38:34 +0300

    update

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,19 +3,17 @@ import React from 'react';
 import { Container, Typography, TextField, Button, Link} from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import logo from "../assets/icon_logo.png";
 const SignIn = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // const username = event.currentTarget.username.value;
     const email = event.currentTarget.email.value;
     const password = event.currentTarget.password.value;
 
     try {
       const response = await axios.post('https://e2e-y8hj.onrender.com/api/users/sign-in', {
-        // username,
         email,
         password,
       }, {
@@ -39,19 +37,24 @@ const SignIn = () => {
 
   return (
     <Container component="main" maxWidth="xs">
-      <Typography component="h1" variant="h5">
+      <Container component="main" maxWidth="xs" sx={{ display: 'flex', alignItems: 'center',  justifyContent:"center", margin:"20px"}} >
+      <img src={logo} alt="Logo" style={{ height: "50px" }} />
+            <Typography
+              variant="body1"
+              color="#170F49"
+              sx={{
+                ml: 1,
+                fontSize: "1.50rem",
+                fontWeight: "bold",
+              }}
+            >
+              Reports
+            </Typography>
+          </Container>
+      <Typography component="h1" variant="h6" fontSize="1rem">
         Sign In
       </Typography>
       <form onSubmit={handleSubmit}>
-        {/* <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          label="Username"
-          name="username"
-          autoComplete="username"
-        /> */}
         <TextField
           margin="normal"
           required
